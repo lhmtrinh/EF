4afe2b0452e4e54fa47ae37186f8635077a7ec73
unknown <trinhlehoangminh2000@gmail.com>
2021-01-22 23:19:47 +0100

    Write test case for StockWrapper

--- src/test/java/model/StockWrapperTest.java
+++ src/test/java/model/StockWrapperTest.java
@@ -3,6 +3,7 @@ package model;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
@@ -29,21 +30,26 @@ public class StockWrapperTest {
 	
 	@Test
 	public void setAdjustedClosesTest() {
-		ArrayList<Double> exp = new ArrayList<Double>(Arrays.asList(51.406097, 43.960255, 48.000862, 49.82, 62.459999,56.66));
-		ArrayList<Double> act = stock.getAdjustedCloses();
-		assertEquals(exp, act);
+		ArrayList<Double> expList = new ArrayList<Double>(Arrays.asList(51.406097, 43.960255, 48.000862, 49.82, 62.459999,56.66));
+		double[] expArr = expList.stream().mapToDouble(Number::doubleValue).toArray();
+		double[] actArr = stock.getAdjustedCloses().stream().mapToDouble(Number::doubleValue).toArray();
+		assertArrayEquals(expArr,actArr, 0.00000001);
+	}
+	@Test
+	public void setReturnRatesTest() {
+		ArrayList<Double> expList = new ArrayList<Double>(Arrays.asList(-0.144843558,0.091915004,0.037898028,0.253713348,-0.092859416));
+		double[] expArr = expList.stream().mapToDouble(Number::doubleValue).toArray();
+		double[] actArr = stock.getReturnRates().stream().mapToDouble(Number::doubleValue).toArray();
+		assertArrayEquals(expArr,actArr, 0.00000001);
 	}
 	@Test
 	public void setReturnRateTest() {
-		stock.setReturnRate();
 		//maximum difference equal to 0.0000000009
 		assertEquals(0.029164681, stock.getReturnRate(),0.0000000009);
 	}
 	@Test
 	public void setRiskRateTest() {
-		stock.setRiskRate();
 		//maximum difference equal to 0.0000000009
 		assertEquals(0.157801082, stock.getRiskRate(),0.0000000009);
-		
 	}
 }
